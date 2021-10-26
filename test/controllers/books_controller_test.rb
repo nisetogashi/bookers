92a7396f0622a1e8a61df91b1ee8c97f6c623273
nisetogashi <ec2-user@ip-172-31-8-39.ec2.internal>
2021-10-26 13:03:13 +0000

    Initial Commit

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get books_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get books_show_url
+    assert_response :success
+  end
+
+  test "should get newedit" do
+    get books_newedit_url
+    assert_response :success
+  end
+
+end
